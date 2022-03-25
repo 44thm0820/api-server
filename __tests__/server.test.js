7fc32a5834bf30fc318fd8fba5c43aa6dd0ebb88
Rey Mercado <reymercado.usa@gmail.com>
2022-03-25 08:46:23 -0700

    wip began writing server.test.js based off Thursdays example 9:48am

---
+++ __tests__/server.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { Test } = require('supertest');
+const { Test } = require('supertest');
+const supertest = require('supertest');
+const app = require('./server/app.js');
+const request = supertest(app);
+
+describe('Testing our REST API', () => {
+  test('Should delete a funko', () => {
+    
+  })
+})
